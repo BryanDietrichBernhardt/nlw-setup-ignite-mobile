5447b2596713bee72e791dd4345431b0969c476c
BryanDietrichBernhardt <bryandbernhardt@gmail.com>
2023-01-22 20:27:10 -0300

    fix: margin-bottom to confirm-button new habit

--- src/screens/New.tsx
+++ src/screens/New.tsx
@@ -96,7 +96,7 @@ export function New() {
 
       <TouchableOpacity
         activeOpacity={0.7}
-        className="w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6"
+        className="w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6 mb-6"
         onPress={handleCreateNewHabit}
       >
         <Feather name="check" size={20} color={colors.white} />
