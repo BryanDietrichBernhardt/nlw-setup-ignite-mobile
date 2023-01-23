51b3775bb6c9efdcf49e1687531b814fe7a54dfb
BryanDietrichBernhardt <bryandbernhardt@gmail.com>
2023-01-22 22:44:42 -0300

    fix: change baseURL to external connections

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://10.0.2.2:3333",
+  baseURL: "http://192.168.1.104:3333", // android physical "http://192.168.1.104:3333" // emulator "http://10.0.2.2:3333",
   proxy: false,
 });
